dfd09b4d76a81c1463b4f0b0b3fd3eeb5224dd6a
Aslan_Agadadashev <aslan.pressa@yandex.ru>
2023-10-05 08:45:48 +0300

    Add first_program.java

---
+++ first_program.java
@@ -0,0 +1,20 @@
+/*
+=========================
+Demo program
+program.java
+=========================
+Многострочный комментарий
+ */
+// Однострочный комм
+public class program {
+
+    public static void main(String[] args) {
+        String s = "By World!";
+        System.out.println(s);
+
+        short age = 10;
+        int salary = 123456;
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
